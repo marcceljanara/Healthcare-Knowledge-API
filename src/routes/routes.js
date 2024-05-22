0892115048d5b3498765553ba23c572b0afda11e
Marccel Janara <nengahyuichiro34@gmail.com>
2024-05-23 01:30:14 +0700

    add middleware verify login

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { getUsers, Register, Login } from "../controllers/user.js";
+import { verifyToken } from "../middleware/verify-token.js";
 
 const router = express.Router();
 
-router.get('/users', getUsers);
+router.get('/users',verifyToken, getUsers);
 router.post('/users', Register);
 router.post('/login', Login);
 
