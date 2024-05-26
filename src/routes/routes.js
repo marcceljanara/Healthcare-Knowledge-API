2189a04057c7da0b9da4356ccba9e3943c1060d5
Marccel Janara <nengahyuichiro34@gmail.com>
2024-05-26 20:27:40 +0700

    Membuat route post create medicine

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,13 +2,19 @@ import express from "express";
 import { getUsers, Register, Login, Logout } from "../controllers/users.js";
 import { verifyToken } from "../middleware/verify-token.js";
 import { refreshToken } from "../controllers/refresh-token.js";
+import { createMedicine } from "../controllers/medicines.js";
+
 
 const router = express.Router();
 
+// Register, Login & Logout route
 router.get('/users',verifyToken, getUsers);
 router.post('/users', Register);
 router.post('/login', Login);
 router.get('/token', refreshToken);
 router.delete('/logout', Logout);
+
+// CRUD medicines route
+router.post('/medicines', createMedicine)
 
 export default router;
