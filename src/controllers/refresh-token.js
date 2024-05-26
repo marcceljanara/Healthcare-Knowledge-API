11e4e645070f476e4878ed0de3dd252479f45acb
Marccel Janara <nengahyuichiro34@gmail.com>
2024-05-26 20:42:13 +0700

    update refresh token based search not arr index 0

--- src/controllers/refresh-token.js
+++ src/controllers/refresh-token.js
@@ -1,27 +1,33 @@
 import Users from "../models/user-models.js";
 import jwt from 'jsonwebtoken';
 
-export const refreshToken = async (req,res) => {
+export const refreshToken = async (req, res) => {
     try {
-        const refreshToken = await req.cookies.refreshToken;
-        if(!refreshToken) return res.sendStatus(401);
-        const user = await Users.findAll({
-            where : {
-                refresh_token : refreshToken
+        const refreshToken = req.cookies.refreshToken;
+        if (!refreshToken) return res.sendStatus(401);
+
+        const user = await Users.findOne({
+            where: {
+                refresh_token: refreshToken
             }
         });
-        if(!user[0]) return res.sendStatus(403);
-        jwt.verify(refreshToken, process.env.REFRESH_TOKEN, (err, decoded) =>{
-            if(err) return res.sendStatus(403);
-            const userId = user[0].id;
-            const name = user[0].id;
-            const email = user[0].id;
-            const accessToken = jwt.sign({userId, name, email},process.env.ACCESS_TOKEN,{
-                expiresIn : '15s'
+
+        if (!user) return res.sendStatus(403);
+
+        jwt.verify(refreshToken, process.env.REFRESH_TOKEN, (err, decoded) => {
+            if (err) return res.sendStatus(403);
+
+            const userId = user.id;
+            const name = user.name;
+            const email = user.email;
+            const accessToken = jwt.sign({ userId, name, email }, process.env.ACCESS_TOKEN, {
+                expiresIn: '15s'
             });
-            res.json({accessToken})
+
+            res.json({ accessToken });
         });
     } catch (error) {
         console.log(error);
+        res.status(500).json({ msg: "Terjadi kesalahan pada server" });
     }
-}
+};
