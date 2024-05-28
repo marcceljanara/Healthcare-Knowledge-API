e084cfbb1a2c638ac547246609f3d923999403cb
Marccel Janara <nengahyuichiro34@gmail.com>
2024-05-28 07:54:07 +0700

    add engine ejs

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ try {
 } catch (error) {
     console.error(error);
 }
-
+app.set('view engine','ejs');
 app.use(cors({credentials : true, origin : 'http://localhost:3000'}))
 app.use(cookieParser());
 app.use(express.json());
